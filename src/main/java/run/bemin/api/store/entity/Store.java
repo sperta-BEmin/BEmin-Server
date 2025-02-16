993f77bf63f47ce4e90119140791b0e3cefc1aa8
LimDae94 <piay801@naver.com>
2025-02-16 23:00:28 +0900

    Feat: #18 가게 엔티티 구현하기
    가게 엔티티 구현이다. 다음으로는 연관관계 매핑이 필요하다.

---
+++ src/main/java/run/bemin/api/store/entity/Store.java
@@ -0,0 +1,70 @@
+package run.bemin.api.store.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "p_store")
+public class Store {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "category_id", nullable = false, updatable = false, unique = true)
+  private UUID id;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "phone", nullable = true)
+  private String phone;
+
+  @Column(name = "minimum_price")
+  private Integer minimumPrice;
+
+  @Column(name = "rating")
+  private Float rating;
+
+  @Column(name = "is_deleted")
+  private Boolean isDeleted;
+
+  @Column(name = "created_by", updatable = false)
+  private String createdBy;
+
+  @Column(name = "updated_by", nullable = true)
+  private String updatedBy;
+
+  @Column(name = "deleted_by", nullable = true)
+  private String deletedBy;
+
+  @Column(name = "created_at", nullable = false)
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at", nullable = true)
+  private LocalDateTime updatedAt;
+
+  @Column(name = "deleted_at", nullable = true)
+  private LocalDateTime deletedAt;
+
+  private Store(String name, String phone, Integer minimumPrice, String createdBy) {
+    this.name = name;
+    this.phone = phone;
+    this.minimumPrice = minimumPrice;
+    this.isDeleted = false;
+    this.createdBy = createdBy;
+    this.createdAt = LocalDateTime.now();
+  }
+
+  public static Store create(String name, String phone, Integer minimumPrice, String createdBy) {
+    return new Store(name, phone, minimumPrice, createdBy);
+  }
+}
