ee8025cdab9891db8b6125f226e75056f3b72224
kim0527 <joo14753@naver.com>
2025-02-19 11:52:07 +0900

    Feat: Product 도메인에서의 Comment API 추가
    - 상품에 대한 comment 변경
    - 상품에 대해 최근에 생성한 comment 조회
      - 생성일 기준 최신순으로 5개를 가져옵니다.

--- src/main/java/run/bemin/api/product/controller/ProductController.java
+++ src/main/java/run/bemin/api/product/controller/ProductController.java
@@ -1,6 +1,7 @@
 package run.bemin.api.product.controller;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -9,10 +10,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import run.bemin.api.comment.dto.CommentDto;
+import run.bemin.api.comment.dto.CommentListDto;
 import run.bemin.api.general.response.ApiResponse;
 import run.bemin.api.product.dto.MessageResponseDto;
 import run.bemin.api.product.dto.ProductRequestDto;
@@ -45,42 +49,58 @@ public class ProductController {
     return ApiResponse.from(HttpStatus.CREATED, "성공", new MessageResponseDto("상품 추가가 완료되었습니다."));
   }
 
-  @GetMapping("/{store_id}/products")
+  @GetMapping("/{storeId}/products")
   public ApiResponse<Page<ProductSearchDto>> getProductsByStoreId(@PathVariable String storeId,
                                                                   @RequestParam("page") int page,
-                                                                  @RequestParam("size") int size)
-  {
+                                                                  @RequestParam("size") int size) {
     UUID storeUUID = UUID.fromString(storeId);
     Store store = validator.isStoreOwner(storeUUID);
 
     Page<ProductSearchDto> products = productService.getProducts(store, page, size);
-    return ApiResponse.from(HttpStatus.OK,"성공",products);
+    return ApiResponse.from(HttpStatus.OK, "성공", products);
   }
 
   @PostMapping("/{storeId}/{productId}")
   public ApiResponse<MessageResponseDto> updateProductDetails(@PathVariable String storeId,
                                                               @PathVariable String productId,
-                                                              @RequestBody UpdateProductDetailDto requestDto)
-  {
+                                                              @RequestBody UpdateProductDetailDto requestDto) {
     UUID storeUUID = UUID.fromString(storeId);
-    Store store = validator.isStoreOwner(storeUUID);
+    validator.isStoreOwner(storeUUID);
 
-    productService.updateProductDetails(productId,requestDto);
-    return ApiResponse.from(HttpStatus.OK,"성공", new MessageResponseDto("상품 상세 설정 변경이 완료되었습니다."));
+    productService.updateProductDetails(productId, requestDto);
+    return ApiResponse.from(HttpStatus.OK, "성공", new MessageResponseDto("상품 상세 설정 변경이 완료되었습니다."));
   }
 
-  @DeleteMapping("/{store_id}/{productId}")
+  @DeleteMapping("/{storeId}/{productId}")
   public ApiResponse<MessageResponseDto> deleteProduct(@PathVariable String storeId,
-                                                       @PathVariable String productId){
+                                                       @PathVariable String productId) {
     UUID storeUUID = UUID.fromString(storeId);
-    Store store = validator.isStoreOwner(storeUUID);
+    validator.isStoreOwner(storeUUID);
     LocalDateTime time = LocalDateTime.now();
 
-    productService.deleteProduct(productId,time);
-    return ApiResponse.from(HttpStatus.OK,"성공", new MessageResponseDto("상품 삭제가 완료되었습니다."));
+    productService.deleteProduct(productId, time);
+    return ApiResponse.from(HttpStatus.OK, "성공", new MessageResponseDto("상품 삭제가 완료되었습니다."));
   }
 
+  @PutMapping("/{storeId}/{productId}/comment")
+  public ApiResponse<MessageResponseDto> updateProductComment(@PathVariable String storeId,
+                                                              @PathVariable String productId,
+                                                              @RequestBody CommentDto commentDto) {
+    UUID storeUUID = UUID.fromString(storeId);
+    validator.isStoreOwner(storeUUID);
 
+    productService.updateComment(productId, commentDto.content());
+    return ApiResponse.from(HttpStatus.OK, "성공", new MessageResponseDto("상품 Comment 설정이 완료되었습니다."));
+  }
 
+  @GetMapping("/{storeId}/{productId}/comment")
+  public ApiResponse<CommentListDto> getProductComment(@PathVariable String storeId,
+                                                       @PathVariable String productId) {
+    UUID storeUUID = UUID.fromString(storeId);
+    validator.isStoreOwner(storeUUID);
+
+    List<String> comments = productService.getRecentProductComment(productId);
+    return ApiResponse.from(HttpStatus.OK, "성공", new CommentListDto(comments));
+  }
 
 }
