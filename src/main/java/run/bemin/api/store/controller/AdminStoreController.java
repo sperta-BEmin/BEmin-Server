4eac9dd2cf220ee587713562079525ef109f1b02
LimDae94 <piay801@naver.com>
2025-02-19 15:59:38 +0900

    Feat: #54 가게 수정 API 컨트롤러, 서비스 구현하기
    가게의 모든 정보를 수정하는 API 입니다.
    
    가게, 주소, 카테고리 변경을 올바르게 반영하기 위해 서비스에서 각 단계별로 로직을 검사합니다. 각 단계마다 실제 엔티티에 반영되는 로직은 엔티티에서 구현했습니다.
    
    1. 가게 조회
    2. 카테고리 검증 및 조회
    3. 기본 가게 정보 업데이트
    4. 가게 주소 업데이트
    5. 카테고리 업데이트
    6. 가게 저장

--- src/main/java/run/bemin/api/store/controller/AdminStoreController.java
+++ src/main/java/run/bemin/api/store/controller/AdminStoreController.java
@@ -2,7 +2,10 @@ package run.bemin.api.store.controller;
 
 import static run.bemin.api.store.dto.StoreResponseCode.STORE_CREATED;
 import static run.bemin.api.store.dto.StoreResponseCode.STORE_FETCHED;
+import static run.bemin.api.store.dto.StoreResponseCode.STORE_UPDATED;
 
+import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -10,6 +13,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,7 @@ import run.bemin.api.general.response.ApiResponse;
 import run.bemin.api.security.UserDetailsImpl;
 import run.bemin.api.store.dto.StoreDto;
 import run.bemin.api.store.dto.request.CreateStoreRequestDto;
+import run.bemin.api.store.dto.request.UpdateAllStoreRequestDto;
 import run.bemin.api.store.service.StoreService;
 
 @RequiredArgsConstructor
@@ -27,6 +32,7 @@ public class AdminStoreController {
   private final StoreService storeService;
 
 
+  @PreAuthorize("not hasRole('CUSTOMER')")
   @GetMapping("/{storeName}")
   public ResponseEntity<ApiResponse<Boolean>> existsStoreName(@PathVariable String storeName) {
     Boolean existsStoreByName = storeService.existsStoreByName(storeName);
@@ -48,5 +54,18 @@ public class AdminStoreController {
         .body(ApiResponse.from(STORE_CREATED.getStatus(), STORE_CREATED.getMessage(), storeDto));
   }
 
+
+  @PutMapping("/{storeId}")
+  public ResponseEntity<ApiResponse<StoreDto>> updateAllStore(
+      @PathVariable("storeId") UUID storeId,
+      @RequestBody @Valid UpdateAllStoreRequestDto requestDto,
+      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+    StoreDto storeDto = storeService.updateAllStore(storeId, requestDto, userDetails);
+
+    return ResponseEntity
+        .status(STORE_UPDATED.getStatus())
+        .body(ApiResponse.from(STORE_UPDATED.getStatus(), STORE_UPDATED.getMessage(), storeDto));
+  }
 
 }
