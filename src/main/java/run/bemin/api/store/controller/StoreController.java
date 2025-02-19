8450fd8f9619f16cc7ea728587433f18cb5f9593
LimDae94 <piay801@naver.com>
2025-02-19 22:37:20 +0900

    Feat: #58 가게 컨트롤러, 리포, 서비스 수정하기
    가게 조회, 수정, 삭제에 필요한 로직을 구현한다.
    컨트롤러는 권한 및 응답을 일관되게 구현하고, 리포지토리에서는 IsDeleted 를 사용했다. 즉, boolean 기본 타입이  아닌 Boolean 타입을 사용하여 자바 빈 규약에 따른 문제를 극복했다.
    
    서비스에서는 각 로직마다 수행되는 절차에 대해 이해를 위해 주석을 기록했다.

--- src/main/java/run/bemin/api/store/controller/StoreController.java
+++ src/main/java/run/bemin/api/store/controller/StoreController.java
@@ -1,8 +1,19 @@
 package run.bemin.api.store.controller;
 
+import static run.bemin.api.store.dto.StoreResponseCode.STORES_FETCHED;
+import static run.bemin.api.store.dto.StoreResponseCode.STORE_FETCHED;
+
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import run.bemin.api.general.response.ApiResponse;
+import run.bemin.api.store.dto.response.GetStoreResponseDto;
 import run.bemin.api.store.service.StoreService;
 
 @RequiredArgsConstructor
@@ -11,6 +22,27 @@ import run.bemin.api.store.service.StoreService;
 public class StoreController {
 
   private final StoreService storeService;
+
+  @PreAuthorize("hasRole('CUSTOMER') or hasRole('MANANGER') or hasRole('MASTER') or hasRole('OWNER')")
+  @GetMapping("/{storeId}")
+  public ResponseEntity<ApiResponse<GetStoreResponseDto>> getStore(
+      @PathVariable UUID storeId) {
+    GetStoreResponseDto getStoreResponseDto = storeService.getStore(storeId);
+
+    return ResponseEntity
+        .status(STORE_FETCHED.getStatus())
+        .body(ApiResponse.from(STORE_FETCHED.getStatus(), STORE_FETCHED.getMessage(), getStoreResponseDto));
+  }
+
+  @PreAuthorize("hasRole('CUSTOMER') or hasRole('MANANGER') or hasRole('MASTER') or hasRole('OWNER')")
+  @GetMapping
+  public ResponseEntity<ApiResponse<List<GetStoreResponseDto>>> getAllStore() {
+    List<GetStoreResponseDto> stores = storeService.getAllStore();
+
+    return ResponseEntity
+        .status(STORES_FETCHED.getStatus())
+        .body(ApiResponse.from(STORES_FETCHED.getStatus(), STORES_FETCHED.getMessage(), stores));
+  }
 
 
 }
