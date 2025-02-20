7bfd635fb86b0c7bdd517cfbb0dfada13ea16372
LimDae94 <piay801@naver.com>
2025-02-21 00:43:55 +0900

    Feat: #82 검색 결과 응답 DTO 생성하기
    최소한의 정보만 전달할 수 있도록 DTO를 생성합니다.

---
+++ src/main/java/run/bemin/api/store/dto/StoreSimpleDto.java
@@ -0,0 +1,26 @@
+package run.bemin.api.store.dto;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import run.bemin.api.store.entity.Store;
+
+public record StoreSimpleDto(
+    UUID id,
+    String name,
+    String phone,
+    List<String> categories
+) {
+  public static StoreSimpleDto fromEntity(Store store) {
+    List<String> categoryNames = store.getStoreCategories().stream()
+        .filter(sc -> !sc.isDeleted())
+        .map(sc -> sc.getCategory().getName())
+        .collect(Collectors.toList());
+    return new StoreSimpleDto(
+        store.getId(),
+        store.getName(),
+        store.getPhone(),
+        categoryNames
+    );
+  }
+}
